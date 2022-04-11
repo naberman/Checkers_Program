b19853cd21294c47b1e08a90903c9dbe408cc5d5
naberman <99360531+naberman@users.noreply.github.com>
2022-04-11 17:50:37 -0400

    Add files via upload

--- Checkers/src/main/java/Checkers/Move.java
+++ Checkers/src/main/java/Checkers/Move.java
@@ -1,5 +1,10 @@
 package Checkers;
 
-public interface Move {
-    
+import Checkers.Board_and_Controller.Piece;
+
+public interface Move extends Comparable<Move> {
+    int[] getStartingLocation();
+    int[] getEndingLocation();
+    Piece getPiece();
+    int getMoveScore();
 }
