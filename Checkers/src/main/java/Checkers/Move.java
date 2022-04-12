d2ee09f603b701d9a86c958441f206bb015085c9
BenjaminNovick <98421827+BenjaminNovick@users.noreply.github.com>
2022-04-12 00:01:21 -0400

    Update Move.java

--- Checkers/src/main/java/Checkers/Move.java
+++ Checkers/src/main/java/Checkers/Move.java
@@ -7,4 +7,5 @@ public interface Move extends Comparable<Move> {
     int[] getEndingLocation();
     Piece getPiece();
     int getMoveScore();
+    void setMeveScore();  // i just added this some times we will need to change or put a score in later
 }
