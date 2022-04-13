d4ce0eeba1c0b40e1ecc08cfc07b2408af8eb320
BenjaminNovick <98421827+BenjaminNovick@users.noreply.github.com>
2022-04-13 15:27:59 -0400

    Update Move.java

--- Checkers/src/main/java/Checkers/Move.java
+++ Checkers/src/main/java/Checkers/Move.java
@@ -5,7 +5,7 @@ import Checkers.Board_and_Controller.Piece;
 public interface Move extends Comparable<Move> {
     int[] getStartingLocation();
     int[] getEndingLocation();
-    //Piece getPiece(); i dont thing this is good for this case it will be contraler tat will find the piece based on the info in this class;
+    //Piece getPiece(); i dont thing this is good for this case it will be contraler that will find the piece based on the info in this class;
     int getMoveScore();
     void setMeveScore();  // i just added this some times we will need to change or put a score in later
 }
