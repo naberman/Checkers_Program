e2a2dda605e2c0ff7198e82b288459cea92c0ae7
Aryeh Berman <amberman@mail.yu.edu>
2022-04-13 16:58:15 -0400

    Implement Piece

---
+++ Checkers/src/main/java/Checkers/Board_and_Controller/PieceImpl.java
@@ -0,0 +1,48 @@
+package Checkers.Board_and_Controller;
+
+public class PieceImpl implements Piece{
+    private Piece.Color color;
+    private int[] location;
+    private boolean isKing;
+
+
+    public PieceImpl(Piece.Color color, int[] location, boolean isKing){
+        this.color = color;
+        this.location = location;
+        this.isKing = isKing;
+    }
+
+
+    @Override
+    public int[] getLocation() {
+        return this.location;
+    }
+
+    @Override
+    public boolean movePiece(int[] location) {
+        this.location = location;
+        return true;
+    }
+
+    @Override
+    public boolean isKing() {
+        return isKing;
+    }
+
+    @Override
+    public boolean promote() {
+        if(!this.isKing){
+            this.isKing = true;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean isRed() {
+        if(this.color == Color.RED){
+            return true;
+        }
+        return false;
+    }
+}
