e11ebbfb36fca4b6a29038600c2676dc8b06e5cf
Benajmin Novick <Bnovick2@mail.yu.edu>
2022-04-13 18:18:39 -0400

    testing

---
+++ Checkers/src/test/Checkers/impl/ControllerImplTest.java
@@ -0,0 +1,36 @@
+package Checkers.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ControllerImplTest {
+
+    @Test
+    public void getAllRedMoves() {
+    }
+
+    @Test
+    public void getAllBlackMoves() {
+    }
+
+    @Test
+    public void getMovesOfPiece() {
+    }
+
+    @Test
+    public void doBlackMove() {
+    }
+
+    @Test
+    public void doRedMove() {
+    }
+
+    @Test
+    public void checkRedMove() {
+    }
+
+    @Test
+    public void checkBlackMove() {
+    }
+}
